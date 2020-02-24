4dfb37cbd3c8f5725f7e78348cd2ab4cfeeb9185
NoteXYX <707997228@qq.com>
2020-02-24 14:53:01 +0800

    ok

--- wordMatrix.py
+++ wordMatrix.py
@@ -18,7 +18,7 @@ def vector( word, length=30 ):
 	wov = []
 	
 	if len(word) <= length:
-		wov = map(char2idx, word)
+		wov = list(map(char2idx, word))
 
 		z =  zeros((length-len(wov), len(boc)+1))
 		wov = concatenate((wov, z), axis=0)
