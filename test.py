4a948e52d30a300e22f6197184b960c165915987
NoteXYX <707997228@qq.com>
2020-02-25 15:35:50 +0800

    ok

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import numpy as np
 import sys
 from chainer import Chain, optimizers, serializers, Variable
 from util import key2value
+from getkp import getkp
 
 ###load arguments
 arg = args.process_command()
@@ -28,9 +29,9 @@ model_url = arg.model
 testing = open(testing_url).readlines()
 dataset = dh.load_data(testing, doc_len, word_len )
 
-x_train = dataset['source'] # array: [每一行文章的序号]
+x_test = dataset['source'] # array: [每一行文章的序号]
 keyword_train = dataset['keyword'] # array: [每一行单词的序号]
-y_train = dataset['target'] # array: [每一行行尾单词的类别 0 or 1]
+y_test = dataset['target'] # array: [每一行行尾单词的类别 0 or 1]
 docs = dataset['docs'] # dict:{文档序号: 文档向量}
 words = dataset['words'] # dict:{单词序号: 单词向量}
 testWords = dataset['testWords'] # array: [每一行行尾的单词]
@@ -59,9 +60,9 @@ slen = lambda a, b, c: c if a-b > c else a-b
 
 for i in range(0, N, batch_size):
 	_ = slen(N, i, batch_size)
-	x = chainer.Variable(np.asarray(list(map(key2value,  x_train[i:i+_], [docs]*_))).astype(np.float32)).reshape(-1, 1, doc_len, word_len, word_dim)
+	x = chainer.Variable(np.asarray(list(map(key2value,  x_test[i:i+_], [docs]*_))).astype(np.float32)).reshape(-1, 1, doc_len, word_len, word_dim)
 	w = chainer.Variable(np.asarray(list(map(key2value, keyword_train[i:i+_], [words]*_))).astype(np.float32)).reshape(-1, 1, doc_len, word_len, word_dim)
-	t = chainer.Variable(np.asarray(y_train[i:i+batch_size]).astype(np.int32))
+	t = chainer.Variable(np.asarray(y_test[i:i+batch_size]).astype(np.int32))
 		#f = chainer.Variable(np.asarray(f_train[i:i+batch_size]))
 	f = []
 
@@ -78,25 +79,47 @@ for i in range(0, N, batch_size):
 print('---\tfin\t\t---')
 
 predicted = [np.argmax(learned_y[i]) for i in range(len(learned_y))]
-curDocIndex = x_train[0]
-curKeywords = []
+curDocIndex = x_test[0]
+curGroundKw = []
+curPreKw = []
 curLine = 0
-while curLine < len(x_train):
-	if x_train[curLine] == curDocIndex:
-		if y_train[curLine] == 1:
-			curKeywords.append(testWords[curLine])
+preNum = 0.0
+groundNum = 0.0
+goodNum = 0.0
+while curLine < len(x_test):
+	if x_test[curLine] == curDocIndex:
+		if y_test[curLine] == 1:
+			curGroundKw.append(testWords[curLine])
+		if predicted[curLine] == 1:
+			curPreKw.append(testWords[curLine])
 		curLine += 1
 	else:
-		curDocIndex = x_train[curLine]
-
-
-
-
-print('---\toutput\t\t---')
-
+		curDocIndex = x_test[curLine]
+		curGroundKp = getkp(indexToDoc[curDocIndex-1], curGroundKw)
+		curPreKp = getkp(indexToDoc[curDocIndex-1], curPreKw)	# if top add here
+		groundNum += len(curGroundKp)
+		preNum += len(curPreKp)
+		for phrase in curPreKp:
+			if phrase in curGroundKp:
+				goodNum += 1
+		curGroundKw = []
+		curPreKw = []
+print('---\toutput result\t\t---')
+precision = goodNum / preNum
+recall = goodNum / groundNum
+f1 = 2 * precision * recall / (precision + recall)
+print('dataset: {}'.format(testing_url))
+print('model:{}'.format(model_url))
+print('precision:{:.2f}, recall:{:.2f}, F1-score:{:.2f}'.format(precision, recall, f1))
 with open('predicted_score', 'w') as f:
 	for i in range(len(predicted)):
 		f.write('{}\n'.format(predicted[i]))
+with open('result/res.txt', 'w', encoding='utf-8') as fres:
+	fres.write('------------------------------------\n')
+	fres.write('dataset: {}\n'.format(testing_url))
+	fres.write('model:{}\n'.format(model_url))
+	fres.write('precision:{:.2f}, recall:{:.2f}, F1-score:{:.2f}\n'.format(precision, recall, f1))
+
 
 
 
